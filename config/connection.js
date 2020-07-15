b7cc36b61670649f102bc78b48982852bb61abda
Skyler Bluth <skylerbluth@Skylers-MacBook.local>
2020-07-14 21:57:55 -0700

    update connection.js

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,11 @@ var connection = mysql.createConnection({
   port: 3306,
   user: "root",
   password: "",
-  database: "cat_db"
+  database: "burgers_db",
 });
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
